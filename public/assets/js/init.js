c260655a76c94a44afcff101da913c1f007d9cbf
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-06-09 21:13:37 -0400

    double click functionality

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -2,39 +2,39 @@ console.log('js loaded');
 
 $(document).ready(function () {
   $('.modal').modal();
-  
+
   $('.sidenav').sidenav();
-  
+
   var mySwiper = new Swiper('.swiper-container');
 
-  $('.liked').dblclick(function () {
 
-    var food_id = $(this).attr('value');
-
-    $.ajax({
-      method: "PUT",
-      url: "/api/foods/undo/" + food_id
-    }).then(function (data) {
-      console.log('unliked');
-    })
-  
-    $(this).toggleClass('liked');
-  })
-  
   $('.swiper-slide').dblclick(function () {
-    var food_id = $(this).attr('value');
-    console.log(food_id);
-    $.ajax({
-      method: "PUT",
-      url: "/api/foods/liked/" + food_id
-    }).then(function (data) {
-      console.log('liked');
-    })
 
-    $(this).toggleClass('liked');    
+    if ($(this).hasClass('liked')) {
+
+      var food_id = $(this).attr('value');
+
+      $.ajax({
+        method: "PUT",
+        url: "/api/foods/undo/" + food_id
+      }).then(function (data) {
+        console.log('unliked');
+      })
+
+      $(this).toggleClass('liked');
+    } else {
+      var food_id = $(this).attr('value');
+      console.log(food_id);
+      $.ajax({
+        method: "PUT",
+        url: "/api/foods/liked/" + food_id
+      }).then(function (data) {
+        console.log('liked');
+      })
+      
+      $(this).toggleClass('liked');
+    }
   });
 
 
-  
-  
 });
